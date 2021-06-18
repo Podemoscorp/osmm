cdbbeebbf1570450152b2b22a08ce378d4a42039
Julio <jn5.guape@gmail.com>
2021-06-18 13:13:05 -0300

    update settings.py

--- osmm/settings.py
+++ osmm/settings.py
@@ -9,7 +9,11 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
+import django_heroku
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+from google.oauth2 import service_account
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-wd19ninr9l!@qv%kk9p04&=$vl@o%%ftp$842oc$v!(kiy#6b0"
+SECRET_KEY = os.environ.get("SECRET_KEY") or "django-insecure-wd19ninr9l!@qv%kk9p04&=$vl@o%%ftp$842oc$v!(kiy#6b0"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = True if os.environ.get("ENV") == "development" else False
 
 ALLOWED_HOSTS = [
     "192.168.0.105",
@@ -44,6 +48,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "core",
 ]
 
 MIDDLEWARE = [
@@ -61,7 +66,7 @@ ROOT_URLCONF = "osmm.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -110,9 +115,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "pt-br"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "America/Sao_Paulo"
 
 USE_I18N = True
 
@@ -125,8 +130,56 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = "/static/"
+STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = BASE_DIR / "static/"
+
+MEDIA_ROOT = BASE_DIR / "data"
+MEDIA_URL = "/data/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+django_heroku.settings(locals())  # Configuraçãos do heroku
+
+# Configurações de email
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
+
+sentry_sdk.init(
+    dsn="https://fefa671909764866a890aec02133a649@o508282.ingest.sentry.io/5658358",
+    integrations=[DjangoIntegration()],
+    traces_sample_rate=1.0,
+    send_default_pii=True,
+)
+
+DEFAULT_FILE_STORAGE = "storages.backends.gcloud.GoogleCloudStorage"
+GS_BUCKET_NAME = os.environ.get("GCS_BUCKET")
+
+STATICFILES_STORAGE = "storages.backends.gcloud.GoogleCloudStorage"
+
+MEDIA_PREFIX = "data"
+
+credenciais = {
+    "type": "service_account",
+    "project_id": os.environ.get("PROJECT_ID"),
+    "private_key_id": os.environ.get("PRIVATE_KEY_ID"),
+    "private_key": str(os.environ.get("PRIVATE_KEY")).replace("\\n", "\n"),
+    "client_email": os.environ.get("CLIENT_EMAIL"),
+    "client_id": os.environ.get("CLIENT_ID"),
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://oauth2.googleapis.com/token",
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/254642947670-compute%40developer.gserviceaccount.com",
+}
+
+GS_CREDENTIALS = service_account.Credentials.from_service_account_info(credenciais)
+
+GS_PROJECT_ID = os.environ.get("PROJECT_ID")
+
+GS_FILE_OVERWRITE = False
